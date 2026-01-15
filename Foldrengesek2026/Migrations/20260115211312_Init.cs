7faad69ef447fd7c414af187617df700b890c895
gludovatza <attila@gludovatz.hu>
2026-01-15 22:48:26 +0100

    Adatbázis és táblák létrehozása migrációval

---
+++ Foldrengesek2026/Migrations/20260115211312_Init.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Foldrengesek2026.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Telepulesek",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nev = table.Column<string>(type: "longtext", nullable: false),
+                    Varmegye = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telepulesek", x => x.ID);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Naplok",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Datum = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Ido = table.Column<TimeSpan>(type: "time(6)", nullable: false),
+                    Magnitudo = table.Column<double>(type: "double", nullable: false),
+                    Intenzitas = table.Column<double>(type: "double", nullable: false),
+                    TelepulesID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Naplok", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Naplok_Telepulesek_TelepulesID",
+                        column: x => x.TelepulesID,
+                        principalTable: "Telepulesek",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Naplok_TelepulesID",
+                table: "Naplok",
+                column: "TelepulesID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Naplok");
+
+            migrationBuilder.DropTable(
+                name: "Telepulesek");
+        }
+    }
+}
