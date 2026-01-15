a35280ca6adbed7fd6be6005d27f69fef04896b6
gludovatza <attila@gludovatz.hu>
2026-01-15 23:03:00 +0100

    CRUD vezérlők és nézeteik

---
+++ Foldrengesek2026/Controllers/NaploController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Foldrengesek2026.Data;
+using Foldrengesek2026.Models;
+
+namespace Foldrengesek2026.Controllers
+{
+    public class NaploController : Controller
+    {
+        private readonly FoldrengesContext _context;
+
+        public NaploController(FoldrengesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Naplo
+        public async Task<IActionResult> Index()
+        {
+            var foldrengesContext = _context.Naplok.Include(n => n.Telepules);
+            return View(await foldrengesContext.ToListAsync());
+        }
+
+        // GET: Naplo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var naplo = await _context.Naplok
+                .Include(n => n.Telepules)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (naplo == null)
+            {
+                return NotFound();
+            }
+
+            return View(naplo);
+        }
+
+        // GET: Naplo/Create
+        public IActionResult Create()
+        {
+            ViewData["TelepulesID"] = new SelectList(_context.Telepulesek, "ID", "Nev");
+            return View();
+        }
+
+        // POST: Naplo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Datum,Ido,Magnitudo,Intenzitas,TelepulesID")] Naplo naplo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(naplo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TelepulesID"] = new SelectList(_context.Telepulesek, "ID", "Nev", naplo.TelepulesID);
+            return View(naplo);
+        }
+
+        // GET: Naplo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var naplo = await _context.Naplok.FindAsync(id);
+            if (naplo == null)
+            {
+                return NotFound();
+            }
+            ViewData["TelepulesID"] = new SelectList(_context.Telepulesek, "ID", "Nev", naplo.TelepulesID);
+            return View(naplo);
+        }
+
+        // POST: Naplo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Datum,Ido,Magnitudo,Intenzitas,TelepulesID")] Naplo naplo)
+        {
+            if (id != naplo.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(naplo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NaploExists(naplo.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TelepulesID"] = new SelectList(_context.Telepulesek, "ID", "Nev", naplo.TelepulesID);
+            return View(naplo);
+        }
+
+        // GET: Naplo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var naplo = await _context.Naplok
+                .Include(n => n.Telepules)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (naplo == null)
+            {
+                return NotFound();
+            }
+
+            return View(naplo);
+        }
+
+        // POST: Naplo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var naplo = await _context.Naplok.FindAsync(id);
+            if (naplo != null)
+            {
+                _context.Naplok.Remove(naplo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NaploExists(int id)
+        {
+            return _context.Naplok.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Foldrengesek2026/Controllers/TelepulesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Foldrengesek2026.Data;
+using Foldrengesek2026.Models;
+
+namespace Foldrengesek2026.Controllers
+{
+    public class TelepulesController : Controller
+    {
+        private readonly FoldrengesContext _context;
+
+        public TelepulesController(FoldrengesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Telepules
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Telepulesek.ToListAsync());
+        }
+
+        // GET: Telepules/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var telepules = await _context.Telepulesek
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (telepules == null)
+            {
+                return NotFound();
+            }
+
+            return View(telepules);
+        }
+
+        // GET: Telepules/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Telepules/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Nev,Varmegye")] Telepules telepules)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(telepules);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(telepules);
+        }
+
+        // GET: Telepules/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var telepules = await _context.Telepulesek.FindAsync(id);
+            if (telepules == null)
+            {
+                return NotFound();
+            }
+            return View(telepules);
+        }
+
+        // POST: Telepules/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Nev,Varmegye")] Telepules telepules)
+        {
+            if (id != telepules.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(telepules);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TelepulesExists(telepules.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(telepules);
+        }
+
+        // GET: Telepules/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var telepules = await _context.Telepulesek
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (telepules == null)
+            {
+                return NotFound();
+            }
+
+            return View(telepules);
+        }
+
+        // POST: Telepules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var telepules = await _context.Telepulesek.FindAsync(id);
+            if (telepules != null)
+            {
+                _context.Telepulesek.Remove(telepules);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TelepulesExists(int id)
+        {
+            return _context.Telepulesek.Any(e => e.ID == id);
+        }
+    }
+}
