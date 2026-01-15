d5433e1a342b5bc71fd80ac538c74b0f47422530
gludovatza <attila@gludovatz.hu>
2026-01-15 21:55:03 +0100

    Modell osztályok

---
+++ Foldrengesek2026/Models/Naplo.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Foldrengesek2026.Models
+{
+    public class Naplo
+    {
+        public int ID { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Dátum")]
+        public DateTime Datum { get; set; }
+        [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "Idő")]
+        public TimeSpan Ido { get; set; }
+        [Required]
+        [Display(Name = "Magnitúdó")]
+        public double Magnitudo { get; set; }
+        [Required]
+        [Display(Name = "Intenzitás")]
+        public double Intenzitas { get; set; }
+
+        // FK + navigáció
+        [Required]
+        [DisplayFormat(NullDisplayText = "Nincs megadva település")]
+        [Display(Name = "Település")]
+        public int TelepulesID { get; set; }
+        public virtual Telepules? Telepules { get; set; } = null!;
+
+    }
+}
---
+++ Foldrengesek2026/Models/Telepules.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Foldrengesek2026.Models
+{
+    public class Telepules
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Település név")]
+        public string Nev { get; set; } = null!;
+        [Required]
+        [Display(Name = "Vármegye")]
+        public string Varmegye { get; set; } = null!;
+
+        // Navigáció: településhez tartozó naplóbejegyzések lekéréséhez:
+        public virtual ICollection<Naplo>? Naplok { get; set; } = new List<Naplo>();
+    }
+}
