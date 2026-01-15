fea895c676e0e1620c01656bd07ccf84ee96e131
gludovatza <attila@gludovatz.hu>
2026-01-15 22:08:33 +0100

    Adat kontextus

---
+++ Foldrengesek2026/Data/FoldrengesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Foldrengesek2026.Data
+{
+    public class FoldrengesContext : DbContext
+    {
+        public DbSet<Models.Telepules> Telepulesek { get; set; } = null!;
+        public DbSet<Models.Naplo> Naplok { get; set; } = null!;
+
+        public FoldrengesContext(DbContextOptions<FoldrengesContext> options) : base(options)
+        {
+        }
+    }
+}
