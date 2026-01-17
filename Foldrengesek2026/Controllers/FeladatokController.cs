d4eba4b107408049d55ed52b84f00a16ebed947a
gludovatza <attila@gludovatz.hu>
2026-01-17 14:46:23 +0100

    Lekérdezési feladatok előkészítése + 2. feladat

---
+++ Foldrengesek2026/Controllers/FeladatokController.cs
@@ -0,0 +1,38 @@
+﻿using Foldrengesek2026.Data;
+using Foldrengesek2026.Models;
+using Foldrengesek2026.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mysqlx.Crud;
+using Org.BouncyCastle.Asn1.X509;
+
+namespace Foldrengesek2026.Controllers
+{
+    public class FeladatokController : Controller
+    {
+        private readonly FoldrengesContext _context;
+
+        public FeladatokController(FoldrengesContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // SELECT nev
+        // FROM telepulesek
+        // WHERE varmegye = "Somogy"
+        // ORDER BY nev
+        public IActionResult Feladat2()
+        {
+            var results = _context.Telepulesek
+                .Where(t => t.Varmegye == "Somogy")
+                .OrderBy(t => t.Nev)
+                .Select(t => t.Nev);
+
+            return View(results);
+        }
+    }
+}
