c4993ac2021ceed4d8e65302a3653c9f525b421c
gludovatza <attila@gludovatz.hu>
2026-01-20 21:51:29 +0100

    Tesztelés megalapozása

---
+++ Foldrengesek2026.Tests/TestDbFactory.cs
@@ -0,0 +1,16 @@
+﻿using Foldrengesek2026.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Foldrengesek2026.Tests;
+
+public static class TestDbFactory
+{
+    public static FoldrengesContext CreateContext(string dbName)
+    {
+        var options = new DbContextOptionsBuilder<FoldrengesContext>()
+            .UseInMemoryDatabase(dbName)
+            .Options;
+
+        return new FoldrengesContext(options);
+    }
+}
