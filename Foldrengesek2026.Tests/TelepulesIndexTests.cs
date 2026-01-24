6596491d91f5397e5cfc6bcdac76e69d03985d6e
gludovatza <attila@gludovatz.hu>
2026-01-24 19:24:09 +0100

    Új (szűrés, lapozás, rendezés) tesztek alapjai

---
+++ Foldrengesek2026.Tests/TelepulesIndexTests.cs
@@ -0,0 +1,64 @@
+﻿using Foldrengesek2026.Data;
+using Foldrengesek2026.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foldrengesek2026.Tests
+{
+    public class TelepulesIndexTests
+    {
+        private static void SeedTelepulesek(FoldrengesContext ctx)
+        {
+            // 25 db: biztosan lesz 3 oldal is pageSize=10 mellett.
+            // Direkt vegyes sorrendben adjuk hozzá.
+            var data = new List<Telepules>
+        {
+            new() { ID = 1,  Nev = "Kaposvár", Varmegye = "Somogy" },
+            new() { ID = 2,  Nev = "Siófok", Varmegye = "Somogy" },
+            new() { ID = 3,  Nev = "Barcs", Varmegye = "Somogy" },
+            new() { ID = 4,  Nev = "Kaposfő", Varmegye = "Somogy" },
+
+            new() { ID = 5,  Nev = "Szekszárd", Varmegye = "Tolna" },
+            new() { ID = 6,  Nev = "Bonyhád", Varmegye = "Tolna" },
+
+            new() { ID = 7,  Nev = "Zalaegerszeg", Varmegye = "Zala" },
+            new() { ID = 8,  Nev = "Kecskemét", Varmegye = "Bács-Kiskun" },
+            new() { ID = 9,  Nev = "Kazincbarcika", Varmegye = "Borsod-Abaúj-Zemplén" },
+            new() { ID = 10, Nev = "Kisbér", Varmegye = "Komárom-Esztergom" },
+            new() { ID = 11, Nev = "Kőszeg", Varmegye = "Vas" },
+            new() { ID = 12, Nev = "Körmend", Varmegye = "Vas" },
+
+            // plusz elemek, hogy biztosan legyen page 2 is
+            new() { ID = 13, Nev = "Győr", Varmegye = "Győr-Moson-Sopron" },
+            new() { ID = 14, Nev = "Szeged", Varmegye = "Csongrád-Csanád" },
+            new() { ID = 15, Nev = "Pécs", Varmegye = "Baranya" },
+            new() { ID = 16, Nev = "Eger", Varmegye = "Heves" },
+            new() { ID = 17, Nev = "Veszprém", Varmegye = "Veszprém" },
+            new() { ID = 18, Nev = "Kaposkeresztúr", Varmegye = "Somogy" },
+            new() { ID = 19, Nev = "Kaposmérő", Varmegye = "Somogy" },
+            new() { ID = 20, Nev = "Kaposújlak", Varmegye = "Somogy" },
+
+            new() { ID = 21, Nev = "Budapest", Varmegye = "Pest" },
+            new() { ID = 22, Nev = "Debrecen", Varmegye = "Hajdú-Bihar" },
+            new() { ID = 23, Nev = "Nyíregyháza", Varmegye = "Szabolcs-Szatmár-Bereg" },
+            new() { ID = 24, Nev = "Miskolc", Varmegye = "Borsod-Abaúj-Zemplén" },
+            new() { ID = 25, Nev = "Sopron", Varmegye = "Győr-Moson-Sopron" },
+        };
+
+            ctx.Telepulesek.AddRange(data);
+            ctx.SaveChanges();
+        }
+
+        private static List<Telepules> ExtractModelList(IActionResult actionResult)
+        {
+            var vr = Assert.IsType<ViewResult>(actionResult);
+            var model = Assert.IsAssignableFrom<List<Telepules>>(vr.Model);
+            return model;
+        }
+
+    }
+}
